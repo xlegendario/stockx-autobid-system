e4045cac6846462f65e885d62ddbf559bab85031
xlegendario <dario.b1996@gmail.com>
2026-04-05 14:25:17 +0200

    Update config.js

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,5 @@
 export const CONFIG = {
   BACKEND_URL: "https://stockx-autobid-system.onrender.com",
-  RUNNER_NAME: "snrkickz-runner"
+  RUNNER_NAME: "snrkickz-resell-runner",
+  ACCOUNT_GROUP_KEY: "main-account"
 };
